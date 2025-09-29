8ddd8ee3205f4dca5a517df485e54f8a35f31682
JNovizki <jnovizki@yahoo.com>
2025-09-29 19:52:40 -0300

    home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,10 +7,64 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter + Material 3"),
-        centerTitle: false,
-      ),
+      appBar: AppBar(title: Text("Flutter + Material 3"), centerTitle: false),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  // Widget build(BuildContext context) {
+  //   return ListView.builder(
+  //     itemCount: appMenuItems.length,
+  //     itemBuilder: (context,i){
+  //       return Text(appMenuItems[i].title);},);
+  // }
+  // Widget build(BuildContext context) { 
+  //   return ListView.builder(
+  //     itemCount: appMenuItems.length,
+  //     itemBuilder: (context, i) {
+  //       final menuItem = appMenuItems[i];
+  //       return ListTile(
+  //         leading: Icon(menuItem.icon),
+  //         title: Text(menuItem.title),
+  //         subtitle: Text(menuItem.subTitle)
+  //       );
+  //     },
+  //   );
+  // }
+  Widget build(BuildContext context) { 
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, i) {
+        final menuItem = appMenuItems[i];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon, color:colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded,color:colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap:() {
+        //TODO navegar a otra pantalla
+      },
     );
   }
 }
