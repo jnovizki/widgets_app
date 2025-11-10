a3d63c8ed3d67685cf6c794ca0e45dfbd5e297bf
JNovizki <jnovizki@yahoo.com>
2025-11-10 20:50:02 -0300

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/ui_controlls/ui_constrolls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiConstrollsScreen extends StatelessWidget {
+  static const name = 'ui_controlls_screen';
+  const UiConstrollsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
